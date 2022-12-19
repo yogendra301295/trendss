17d06c80943441314a03114f6eda7398e7fe4d96
yogendra301295 <yogendra301295@gmail.com>
2022-12-19 10:14:55 -0500

    route styledC header nav added

--- src/App.js
+++ src/App.js
@@ -7,11 +7,36 @@ import About from "./About";
 import SingleProduct from "./SingleProduct"
 import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
+import { GlobalStyle } from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import Header from "./Component/Header";
 
 const App = () => {
-  return (
+ const theme = {
+  colors: {
+    text: "rgba(29 ,29 ,29 .8)",
+    white: "#fff",
+    black: "#212529",
+    helper: "#8490ff",
+    bg: "#F6F8FA",
+    footer_bg: "0a1435",
+    btn: "rgb(98, 84, 243, 0.5)",
+    hr: "#ffffff",
+    gradient: "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+    shadowSupport: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
 
+  },
+  media:{
+    mobile : "768px",
+    tab: "998px",
+  }
+ };
+
+  return (
+<ThemeProvider theme={theme}>
     <Router> 
+    <GlobalStyle/>
+    <Header /> 
       <Routes>
         <Route path="/" element={<Home/>} /> 
         <Route path="/contact" element={<Contact /> } /> 
@@ -20,10 +45,10 @@ const App = () => {
         <Route path="/singleProduct/:id" element={<SingleProduct /> } /> 
         <Route path="/cart" element={<Cart /> } /> 
         <Route path="*" element={<ErrorPage /> } /> 
-
       </Routes>
     </Router>
-
+    </ThemeProvider>
+    
   );
 };
 
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Home = () => {
   return (
-    <div>
-     
-    Home</div>
+    <Wrapper className='test'> Home</Wrapper>
   )
-}
+};
 
-export default Home
+const Wrapper = styled.section`
+background-color: ${({theme})=>theme.colors.bg};
+width:20rem;
+height:20rem;
+
+`;
+
+export default Home;
