87654db6ad9372240f1d684620d914390b721bc6
yogendra301295 <yogendra301295@gmail.com>
2022-12-25 06:56:05 -0500

    Feature page price converted in Indian rupee

---
+++ src/Component/helpers/FormatePrice.js
@@ -0,0 +1,11 @@
+ const FormatePrice =({price}) =>{
+
+    return Intl.NumberFormat("en-IN",{
+        style :"currency",
+        currency : "INR",
+        maximumFractionDigits:2,
+    }).format(price/100);
+
+ }
+
+export default FormatePrice
